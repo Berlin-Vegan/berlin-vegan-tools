a256b1977462e74575a0553d8fd57ac8e77a2016
Marco Eckstein <marco.eckstein@gmx.de>
2014-11-10 19:55:20 +0100

    Refactoring: Moved class PlaceMark to dedicated file

---
+++ src/main/java/org/berlinvegan/generators/PlaceMark.java
@@ -0,0 +1,27 @@
+package org.berlinvegan.generators;
+
+class PlaceMark {
+    private String name;
+    private String description;
+    private String latPos;
+    private String longPos;
+
+    PlaceMark(String name, String description, String latPos, String longPos) {
+        this.name = name;
+        this.description = description;
+        this.latPos = latPos;
+        this.longPos = longPos;
+    }
+    
+    public String toKMLFormat() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("<Placemark>\n");
+        builder.append("<name>").append(name).append("</name>\n");
+        builder.append("<description>").append(description).append("</description>\n");
+        builder.append("<Point><coordinates>");
+        builder.append(longPos).append(",").append(latPos).append(",0");
+        builder.append("</coordinates></Point>\n");
+        builder.append("</Placemark>\n");
+        return builder.toString();
+    }
+}
