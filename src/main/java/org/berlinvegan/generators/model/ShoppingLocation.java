6080120ea3f78daed01395e048bdcdd2b55ccf87
Marco Eckstein <marco.eckstein@gmx.de>
2018-11-28 21:35:39 +0100

    Support shops in a broader sense

--- src/main/java/org/berlinvegan/generators/model/ShoppingLocation.java
+++ src/main/java/org/berlinvegan/generators/model/ShoppingLocation.java
@@ -25,15 +25,35 @@ public class ShoppingLocation extends Location {
         if (value == null) {
             throw new RuntimeException("Tags must be set.");
         }
-        List<String> tags = Arrays.stream(value.split(",")).map(String::trim).collect(toList());
+        List<String> tags =
+            Arrays.stream(value.split(","))
+                .map(String::trim)
+                .map(tag -> {
+                    switch (tag) {
+                        case "food": return "foods";
+                        case "fashion": return "clothing";
+                        case "cosmetics": return "toiletries";
+                        default: return tag;
+                    }
+                })
+                .collect(toList());
         List<String> allowedTags =
-            Arrays.asList("foods", "clothing", "toiletries", "supermarket");
+            Arrays.asList(
+                "foods",
+                "clothing",
+                "toiletries",
+                "supermarket",
+                "hairdressers",
+                "fitness",
+                "tattoostudio",
+                "accommodation"
+            );
         for (String tag : tags) {
             if (!allowedTags.contains(tag)) {
                 throw new RuntimeException("Found illegal tag '" + tag + "'.");
             }
         }
-        if (tags.size() < 1 || tags.size() > 4) {
+        if (tags.size() < 1 || tags.size() > 8) {
             throw new RuntimeException("Found " + tags.size() + " tags. (expected 1 to 4)");
         }
         return tags.toArray(new String[0]);
